e03617060c60e94ebea807004e2f9a9261284035
SATHISH S <sathish2462003@gmail.com>
2025-07-20 22:46:26 +0530

    fix: update project ID and credentials in configuration

--- config/__init__.py
+++ config/__init__.py
@@ -4,17 +4,17 @@ from config.rss_sources import rss_sources
 
 class Config:
     TYPE = "service_account"
-    PROJECT_ID = "theneural-ai-news"
+    PROJECT_ID = "eng-pulsar-466105-g5"
 
     class Credentials:
-        CLIENT_ID = "107320439288964420848"
+        CLIENT_ID = "115510078425093279793"
         CLIENT_EMAIL = st.secrets["client_email"]
-        PRIVATE_KEY_ID = "247212d8193e448e92aa229295baefefa58e1dd7"
+        PRIVATE_KEY_ID = "f951f5767fc7138732a890cfac27fc75b3ee37a7"
         PRIVATE_KEY = st.secrets["private_key"]
         AUTH_URI = "https://accounts.google.com/o/oauth2/auth"
         TOKEN_URI = "https://oauth2.googleapis.com/token"
         AUTH_PROVIDER_X509_CERT_URL = "https://www.googleapis.com/oauth2/v1/certs"
-        CLIENT_X509_CERT_URL = "https://www.googleapis.com/robot/v1/metadata/x509/news-api%40theneural-ai-news.iam.gserviceaccount.com"
+        CLIENT_X509_CERT_URL = "https://www.googleapis.com/robot/v1/metadata/x509/the-neural-872%40eng-pulsar-466105-g5.iam.gserviceaccount.com"
         UNIVERSE_DOMAIN = "googleapis.com"
 
     class LLM:
